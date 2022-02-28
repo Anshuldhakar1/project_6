f7d696520f17a40c7c6e212bf2f2b8952d5fb45c
Anshul <anshuldhakar03@gmail.com>
2022-02-28 17:47:37 +0530

    site topbar done

--- critic_zone/urls.py
+++ critic_zone/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('', views.index_view, name="index"),
+    path(                                             
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
